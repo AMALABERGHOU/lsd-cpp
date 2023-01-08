3bfcb752f7f8713c86c6e7356d0edad61a1f5db5
AMALABERGHOU <92234229+AMALABERGHOU@users.noreply.github.com>
2023-01-08 16:19:53 +0100

    Create liste.h

---
+++ TP01/liste.h
@@ -0,0 +1,24 @@
+#ifndef LISTE_H
+# define LISTE_H
+
+struct node {
+    int element;
+    struct node * next;
+    // int * prev;
+};
+typedef struct node node;
+typedef node * stackList;
+
+//fucntions prototypes:
+stackList stack_empty();
+void stackPrint(stackList S);
+void push(stackList * S , int x);
+void pop(stackList * S);
+int lengthList(stackList L);
+void deleteFirst(stackList* S);
+stackList copy(stackList S);
+stackList slice(stackList S,int i,int j);
+int check_is_in(int x, stackList L);
+stackList removeDuplicates(stackList L);
+
+#endif
